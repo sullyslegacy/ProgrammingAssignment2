d232341d20c4bc73e8ead1d2252f51d7b27331df
sullyslegacy <sullyslegacy@yahoo.com>
2016-01-17 21:59:26 -0500

    Code Added
    Filled in dummy functions with code similar to example vector cache

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+##The logic in the functions below is very similar to the logic
+#from the examples provided
+#mat = matrix(c(1, 0, 1, 2, 4, 0, 3, 5, 6), nrow = 3, ncol = 3)
+#temp = makeCacheMatrix(mat)
+#cacheSolve(temp)
 
-## Write a short comment describing this function
-
+## This function contains basic getters and setters for matrix values
+#As well as getting and setting the inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) m <<- inverse
+    getInverse <- function() m
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+##Check if the created cached matrix has inverse computed already
+#if it does return the cached matrix
+#else compute inverse and cache it
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getInverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setInverse(m)
+    m
 }
